f8e6d0e74376d045decc41e8b1ae7d395816dd90
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-29 23:50:31 +0800

    main

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
 // get username and room from URL
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
@@ -10,9 +13,15 @@ const socket = io();
 // join chat room
 socket.emit("joinRoom", { username, room });
 
+// get room users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
 // message from server
 socket.on("message", (message) => {
-  console.log(message);
+//   console.log(message);
   outputMessage(message);
 
   //scroll down
@@ -45,4 +54,16 @@ function outputMessage(message) {
     </p>`;
 
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+// add room name to DOM
+function outputRoomName(room) {
+  roomName.innerHTML = room;
+}
+
+// add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = `${users
+    .map((user) => `<li>${user.username}</li>`)
+    .join("")}`;
 }
--- server.js
+++ server.js
@@ -35,10 +35,10 @@ io.on("connection", (socket) => {
       );
 
     //   send users and room info
-      io.to(user.room).emit("roomUsers", {
-          room: user.room,
-          user: getRoomUsers(user.room)
-      })
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      user: getRoomUsers(user.room),
+    });
   });
 
   // listen for chat message
@@ -49,12 +49,17 @@ io.on("connection", (socket) => {
 
   // runs when client disconnects
   socket.on("disconnect", () => {
-    const user = userLeave(socket.id); 
+    const user = userLeave(socket.id);
     if (user) {
       io.to(user.room).emit(
         "message",
         formatMessage(botName, `${user.username}  has left the chat`)
       );
+      //   send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        user: getRoomUsers(user.room),
+      });
     }
   });
 });
